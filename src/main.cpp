64ddacf3e19778d080b94ed5a3a0b813832ebcad
alexmelk <alexmelk@yandex.ru>
2022-09-11 19:47:24 +0300

    Добавлена библиотека управления панелью

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,69 @@
 #include <Arduino.h>
-
 #include <SmartHomeCore.h>
+#include <Adafruit_NeoPixel.h>
+
 using namespace shCore;
 
-void getTemperature()
+float brtn = 1000;
+uint8_t ledPin = D7;
+uint8_t stripLength = 64;
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(stripLength, ledPin, NEO_RGB + NEO_KHZ800);
+
+void display(int row, int column, int green, int red, int blue)
 {
-  sendToServer(200, "text/html", "10");
-  blink(1, 100);
+  if (row == 0)
+  {
+    strip.setPixelColor(column, green, red, blue);
+  }
+  if ((row % 2) == 0)
+  {
+    strip.setPixelColor(8 * row + column, green, red, blue);
+  }
+  else
+  {
+    strip.setPixelColor(8 * row + (7 - column), green, red, blue);
+  }
 }
 
 void setup(void)
 {
-  setSSIDwifiAP("WiFi Toggle");
-  registrateEvent("/getTemperature", getTemperature);
-
+  setSSIDwifiAP("✅ wifi-led-panel ✅");
+  setPassWiFiAP("3866210000");
+  // registrateEvent("/off", off);
+  // registrateEvent("/on", on);
+  Serial.begin(9600);
+  strip.begin();
+  strip.clear();
   coreInit();
+}
+
+int micVolume()
+{
+  int mic = analogRead(A0);
+  int minMic = 500;
+  if (mic < minMic)
+    mic = minMic;
+  return map(mic, minMic, 1024, 0, 7);
+}
+
+void displayPick(int value)
+{
+  strip.clear();
+  for (int i = 0; i <= value; i++)
+  {
+    display(7 - i, 1, 10, 100, 10);
+  }
+  strip.show();
 }
 
 void loop(void)
 {
   coreHandle();
-  delay(1);
+  Serial.println(micVolume());
+  displayPick(micVolume());
+  delay(10);
+}
+
+void displayPicks(int value)
+{
 }
